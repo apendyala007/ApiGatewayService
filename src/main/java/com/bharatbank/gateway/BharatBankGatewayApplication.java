81e7975d1eb0e773badf8efd1b0a8abba5794348
bhanubilla <bhanubillakl@gmail.com>
2026-03-29 16:30:18 +0530

    BB-002: port changes

--- src/main/java/com/bharatbank/gateway/BharatBankGatewayApplication.java
+++ src/main/java/com/bharatbank/gateway/BharatBankGatewayApplication.java
@@ -2,6 +2,7 @@ package com.bharatbank.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(excludeName = {
     "org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration",
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     "org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration",
     "org.springframework.cloud.client.discovery.simple.reactive.SimpleReactiveDiscoveryClientAutoConfiguration"
 })
+@EnableDiscoveryClient
 public class BharatBankGatewayApplication {
 
     public static void main(String[] args) {
